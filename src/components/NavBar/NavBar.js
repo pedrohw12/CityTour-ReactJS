58fbf5e78b91fa85784a5d381e97c8a0e965ebf0
Pedro Wanderley <peu-zico>
2019-11-12 21:50:43 -0300

    First commit

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './NavBar.scss';
+import logo from '../../logo.png'
+
+export default function NavBar() {
+  return (
+    <nav className="navbar">
+      <img src={logo} alt="city tours logo" />
+      <ul className="nav-links">
+        <li>
+          <a href="/" className="nav-link">
+            home
+          </a>
+        </li>
+        <li>
+          <a href="/" className="nav-link">
+            about
+          </a>
+        </li>
+        <li>
+          <a href="/" className="nav-link active">
+            tours
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
